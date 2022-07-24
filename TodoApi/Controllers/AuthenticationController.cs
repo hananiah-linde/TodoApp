a32109d76578ad13506e233eb9079281b8230995
hananiah-linde <57416903+hananiah-linde@users.noreply.github.com>
2022-07-24 13:24:23 -0500

    Add project files.

---
+++ TodoApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace TodoApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+
+    private readonly IConfiguration _config;
+
+    public AuthenticationController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public record AuthenticationData(string? UserName, string? Password);
+    public record UserData(int Id, string? UserName, string? FirstName, string? LastName);
+
+    [HttpPost("login")]
+    [AllowAnonymous]
+    public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
+    {
+        var user = ValidateCredentials(data);
+
+        if (user == null) return Unauthorized();
+
+        string token = GenerateToken(user);
+
+        return Ok(token);
+    }
+
+    private string GenerateToken(UserData user)
+    {
+        var secretKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config.GetValue<string>("Authentication:SecretKey")));
+        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        List<Claim> claims = new();
+
+        claims.Add(new(JwtRegisteredClaimNames.Sub, user.Id.ToString()));
+        claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.UserName!));
+        claims.Add(new(JwtRegisteredClaimNames.GivenName, user.FirstName!));
+        claims.Add(new(JwtRegisteredClaimNames.FamilyName, user.LastName!));
+        var token = new JwtSecurityToken(
+            _config.GetValue<string>("Authentication:Issuer"),
+            _config.GetValue<string>("Authentication:Audience"),
+            claims,
+            expires: DateTime.UtcNow.AddMinutes(10),
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private UserData? ValidateCredentials(AuthenticationData data)
+    {
+        //NOT PRODUCTION CODE
+        if(CompareValues(data.UserName, "admin") && 
+           CompareValues(data.Password, "password"))
+        {
+            return new UserData(1, data.UserName, "Hananiah", "Linde");
+        }
+
+        return null;
+    }
+
+    private bool CompareValues(string? actual, string expected)
+    {
+        if (actual is not null)
+        {
+            return actual.Equals(expected);
+        }
+        return false;
+    }
+}
---
+++ TodoApi/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = 
+        };
+
+    });
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
